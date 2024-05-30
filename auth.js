c176a7d1b939d920b162e7902b2852a31e67931e
Emmanuel Lafenwa <leoemaxie@gmail.com>
2024-05-30 02:28:23 +0100

    Add base-64 dependency and fix authMiddleware bug

--- auth.js
+++ auth.js
@@ -1,10 +1,21 @@
+import base64 from "base-64";
+
+function decodeCredentials(authHeader) {
+  const encodedCredentials = authHeader.trim().replace(/Basic\s+/i, "");
+  const decodedCredentials = base64.decode(encodedCredentials);
+
+  return decodedCredentials.split(":");
+}
+
 export default function authMiddleware(req, res, next) {
-	const [ username, password ] = req.headers.authorization || "";
+  const [username, password] = decodeCredentials(
+    req.headers.authorization || ""
+  );
 
-	if (username == "admin" && password == "password") {
-		next();
-	}
+  if (username == "admin" && password == "password") {
+    return next();
+  }
 
-	res.set("WWW-Authenticate", "Basic realm=Access to unathorized page");
-	return res.status(401).json({error: "Unathorized"});
+  res.set("WWW-Authenticate", "Basic realm=index");
+  res.status(401).json({ error: "Unathorized" });
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+import * as fs from "node:fs";
 import express from "express";
-import fs;
-import authMiddleware from "./auth"; 
+import authMiddleware from "./auth.js";
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(authMiddleware);
 app.get("/view", (req, res) => {
   if (fs.existsSync("memories.json")) {
     const file = fs.readFileSync("memories.json");
-    return res.status(200).json(file);
+    return res.status(200).json(JSON.parse(file));
   }
 
   return res.status(200).send("You do not have any memories yet!");
@@ -19,27 +19,25 @@ app.get("/view", (req, res) => {
 app.post("/new", (req, res) => {
   const { id, content } = req.body;
 
-  if (fs.existsSync("memories.json")) {
-
-    const file = fs.readFileSync("memories.json");
-    const memories = JSON.parse(file);
-
+  if (!id || !content || Object.keys(req.body).length != 2) {
+    return res.status(400).json({ error: "Bad Request" });
   }
 
-  return res.status(200).send("You do not have any memories yet!");
-})
+  let memories = { id, content };
+
+  if (fs.existsSync("memories.json")) {
+    const file = fs.readFileSync("memories.json");
+    memories = [...JSON.parse(file), memories];
+  }
+
+  fs.writeFileSync("memories.json", JSON.stringify(memories));
+  res.status(201).json({ message: "Memory added successfully" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
 
 app.listen(3000, () => {
   console.log("server listening at port 3000");
 });
-
-
-
-
-
-
-
-app.listen(3000, () => {
-  console.log("server listening at port 3000");
-});
-
