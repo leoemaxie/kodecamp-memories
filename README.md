4500f2b0438fddfe66b0d4481885dada7de2fec3
Emmanuel Lafenwa <leoemaxie@gmail.com>
2024-05-30 09:56:11 +0100

    chore: Refactor code to improve memory retrieval and storage

---
+++ README.md
@@ -0,0 +1,22 @@
+# Memories API
+
+API to post and view memories.
+
+## Installation
+
+1. Clone the repository.
+2. Install the dependencies using the following command:
+
+   ```shell
+   npm install
+   ```
+
+## Usage
+
+To start the server, run the following command:
+
+```shell
+    npm run start
+```
+
+## Endpoints
--- index.js
+++ index.js
@@ -8,9 +8,22 @@ app.use(express.json());
 app.use(authMiddleware);
 
 app.get("/view", (req, res) => {
+  const { id } = req.query;
+
   if (fs.existsSync("memories.json")) {
     const file = fs.readFileSync("memories.json");
-    return res.status(200).json(JSON.parse(file));
+    const memories = JSON.parse(file);
+
+    if (id) {
+      const memory = memories.find((memory) => memory.id == id);
+
+      if (!memory) {
+        return res.status(404).json({ error: "Memory not found" });
+      }
+      return res.status(200).json(memory);
+    }
+
+    return res.status(200).json(memories);
   }
 
   return res.status(200).send("You do not have any memories yet!");
@@ -23,11 +36,16 @@ app.post("/new", (req, res) => {
     return res.status(400).json({ error: "Bad Request" });
   }
 
-  let memories = { id, content };
+  if (
+    typeof content != "string" ||
+    (typeof id != "number" && typeof id != "string")
+  ) {
+    return res.status(400).json({ error: "Bad Request" });
+  }
 
   if (fs.existsSync("memories.json")) {
     const file = fs.readFileSync("memories.json");
-    memories = [...JSON.parse(file), memories];
+    memories = [...JSON.parse(file), { id, content }];
   }
 
   fs.writeFileSync("memories.json", JSON.stringify(memories));
