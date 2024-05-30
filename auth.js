638f2db785c38b8f73468e78627c7e1d239a8dbd
Emmanuel Lafenwa <103007301+leoemaxie@users.noreply.github.com>
2024-05-30 20:12:40 +0100

    Changed authentication error message

--- auth.js
+++ auth.js
@@ -17,5 +17,5 @@ export default function authMiddleware(req, res, next) {
   }
 
   res.set("WWW-Authenticate", "Basic realm=index");
-  res.status(401).json({ error: "Unathorized" });
+  res.status(401).json({ error: "Authentication required" });
 }
